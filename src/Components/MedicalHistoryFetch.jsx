1c9f3da3411189bf098bf3f483a1334c0c6ce64c
Jeremy Montes <jeremymontes@pursuit.org>
2024-06-07 15:49:02 -0400

    Added loading state and error state to Medical History Fetch and Med History Fetch

--- src/Components/MedicalHistoryFetch.jsx
+++ src/Components/MedicalHistoryFetch.jsx
@@ -1,60 +1,86 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLoginDataProvider } from "../Components/LoginProvider";
 import EditMedicalHistory from "./EditMedicalHistory";
 import '../Styles/MedicalHistoryFetch.css'
 
-const MedicalHistoryFetch = ({setShowMedicineCabinet}) => {
+const MedicalHistoryFetch = ({ setShowMedicineCabinet }) => {
     const { API, token } = useLoginDataProvider();
     const [medHistory, setMedHistory] = useState([]);
     const [editMode, setEditMode] = useState(false);
-
-    useEffect(() => {
-        fetch(`${API}/users/medical`, {
-            headers: {
-                "Authorization": token
-            }
-        })
-        .then((res) => res.json())
-        .then((res) => setMedHistory(res))
-        .catch((error) => console.error('Error fetching medical history:', error));
-    }, [API, token, medHistory]);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false)
 
     const handleEditClick = () => {
         setEditMode(true);
     };
 
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            setError("");
+
+
+            const response = await fetch(`${API}/users/medical`, {
+                headers: {
+                    "Authorization": token
+                }
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setMedHistory(data);
+            } else {
+                throw new Error(data.error);
+            }
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [API, token])
+
+
     const handleEditCancel = () => {
         setEditMode(false);
     };
 
-    return (
-        <div className='medicalBody'>
-            {!editMode && (
-                <>
-                    {medHistory.map((person) => (
-                        <div key={person.medical_id} className="personCard">
-                            <h2>Medication: {person.medication}</h2>
-                            <h2>Allergies: {person.allergies}</h2>
-                            <h2>Blood Type: {person.blood_type}</h2>
-                            <h2>Medical History: {person.medical_history}</h2>
-                            <div>
-                            <img  onClick={handleEditClick} className="editButtonMed" src='../DarkButton.svg' alt="Edit Emergency Contact" />   
-                            </div>
-                        </div>
-                    ))}
-                </>
-            )}
+    const renderContent = () => {
 
-            {editMode && (
-                <EditMedicalHistory
-                    medHistory={medHistory}
-                    setEditMode={setEditMode}
-                    handleEditCancel={handleEditCancel}
-                    setShowMedicineCabinet={setShowMedicineCabinet}
-                />
-            )}
-        </div>
-    );
+        return (
+            <div className='medicalBody'>
+                {!editMode && medHistory.length > 0 && medHistory && (
+                    <>
+                        {medHistory.map((person) => (
+                            <div key={person.medical_id} className="personCard">
+                                <h2>Medication: {person.medication}</h2>
+                                <h2>Allergies: {person.allergies}</h2>
+                                <h2>Blood Type: {person.blood_type}</h2>
+                                <h2>Medical History: {person.medical_history}</h2>
+                                <div>
+                                    <img onClick={handleEditClick} className="editButtonMed" src='../DarkButton.svg' alt="Edit Emergency Contact" />
+                                </div>
+                            </div>
+                        ))}
+                    </>
+                )}
+
+                {editMode && (
+                    <EditMedicalHistory
+                        medHistory={medHistory}
+                        setEditMode={setEditMode}
+                        handleEditCancel={handleEditCancel}
+                        setShowMedicineCabinet={setShowMedicineCabinet}
+                    />
+                )}
+            </div>
+        );
+    }
+    return <div className="Med">{renderContent()}</div>
 };
 
 export default MedicalHistoryFetch;
--- src/Pages/MedHistoryFetch.jsx
+++ src/Pages/MedHistoryFetch.jsx
@@ -9,35 +9,63 @@ import "../Styles/MedHistoryFetch.css"
 const MedHistoryFetch = () => {
     const { API, user, token } = useLoginDataProvider()
     const [medHistory, setMedHistory] = useState([]);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false)
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            setError("");
+
+
+            const response = await fetch(`${API}/users/medical`, {
+                headers: {
+                    "Authorization": token
+                }
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setMedHistory(data);
+            } else {
+                throw new Error(data.error);
+            }
+        } catch (err) {
+            setError(err.message);
+            console.log("error:" , error)
+        } finally {
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        fetch(`${API}/users/medical`, {
-            headers: {
-                "Authorization": token
-            }
-        })
-        .then((res) => res.json())
-        .then((res) => setMedHistory(res))
-        .catch((error) => console.error('Error fetching medical history:', error));
-    }, [API, token]);
+        fetchData();
+    }, [API,token])
+
 
     return (
         <div className="medCab">
             <h2>Medicine Cabinet</h2>
-
+    
             <div className="mCard">
-        {medHistory.length === 0 ? (
-
-            <MedicalHistoryPage />
-
-            ) : (
-            <div className="historyFetch">
-            <p> <MedicalHistoryFetch /> </p>
-            <Link to="/users/home"><button className="backB">Back</button></Link>
+                {loading ? (
+                    <div>
+                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c4954169321565.5b7d0cbe74d11.gif" alt="loading" style={{ width: '500px', height: '500px', borderRadius: '80px'}} />{" "}
+                    </div>
+                ) : (
+                    <>
+                        {medHistory && medHistory.length > 0 && Array.isArray(medHistory) ? (
+                            <div className="historyFetch">
+                                <p> <MedicalHistoryFetch /> </p>
+                                <Link to="/users/home"><button className="backB">Back</button></Link>
+                            </div>
+                        ) : (
+                            <MedicalHistoryPage />
+                        )}
+                    </>
+                )}
             </div>
-            )
-    }
-            </div>       
         </div>
     );
 };
